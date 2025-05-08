5c23666c55bd6b66b95d71df948efb3ec8caf68d
Navpreet Singh <57364214+NavpreetSinghDhillon@users.noreply.github.com>
2025-05-08 11:51:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles.css';
 
-// Get the root element
-const container = document.getElementById("root");
-
-// Create a root
+const container = document.getElementById('root');
 const root = createRoot(container);
-
-// Render the app
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
